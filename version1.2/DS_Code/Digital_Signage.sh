99d8a9decded9193fcae2612a272e43b58720d61
GS <sgauravsharma059@gmail.com>
2015-10-12 10:22:17 -0400

    Update Digital_Signage.sh

--- version1.2/DS_Code/Digital_Signage.sh
+++ version1.2/DS_Code/Digital_Signage.sh
@@ -3,7 +3,6 @@
 #__copyright__  = "Copyright 2014, School of Public Health, University of Maryland"  #
 #__department__ = "Telecommunications"                                               #
 #__credits__    = "Gaurav Sharma"                                                    #
-#__email__      = "gaurav10@umd.edu", "sgauravsharma059@gmail.com"                   #
 #__status__     = "Implemented"                                                      #
 #__Purpose__    = "Copies files from local folder into slidesshow.jpg at intervals"  #
 #__Platform__	= "Python"                                                           #
